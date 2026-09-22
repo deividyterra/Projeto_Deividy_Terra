9abd6641937268c894d716ecfbad4eb13273bb45
Deividy Terra <deividyterra2@gmail.com>
2026-09-21 23:18:41 -0300

    Salvando todas as Alterações

---
+++ D.java
@@ -0,0 +1,16 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+
+    public void MD3(){
+        System.out.println("MD3");
+    }
+}
